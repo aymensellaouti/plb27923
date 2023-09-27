5c8328dea3d9d209aa05830ade34a25552ad1889
aymen2782 <aymen.sellaouti@gmail.com>
2023-09-27 11:17:17 +0100

    binding {{interpolation}} [propertyBinding] (eventBinding) et la ref via #

---
+++ src/app/components/first/first.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-first",
+  templateUrl: "./first.component.html",
+  styleUrls: ["./first.component.css"],
+})
+export class FirstComponent {
+  name = "first";
+  buttonText = "show";
+  isHidden = true;
+  message = "";
+  constructor() {
+    setTimeout(() => {
+      this.name = "newName";
+    }, 1500);
+  }
+  showHide() {
+    this.isHidden = !this.isHidden;
+    this.buttonText = this.isHidden ? "show" : "hide";
+  }
+  changeMessage(content: string) {
+    this.message = content;
+  }
+}
