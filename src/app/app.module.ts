6fdf997b893116d2324f5cd9a386bd2d15a316e0
aymen2782 <aymen.sellaouti@gmail.com>
2023-09-27 13:33:59 +0100

    add template files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { FirstComponent } from './components/first/first.component';
-import { SecondComponent } from './components/second.component';
-import { ColorComponent } from './components/color/color.component';
+import { FirstComponent } from "./components/first/first.component";
+import { SecondComponent } from "./components/second.component";
+import { ColorComponent } from "./components/color/color.component";
+import { TwoComponent } from "./components/two/two.component";
+import { FormsModule } from "@angular/forms";
+import { RotatingCardComponent } from './components/rotating-card/rotating-card.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, SecondComponent, ColorComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ColorComponent,
+    TwoComponent,
+    RotatingCardComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
